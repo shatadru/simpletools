6a94acbda2a8c02363b100eb17fb52699d316e6f
Shatadru Bandyopadhyay <shatadru1@gmail.com>
2019-12-12 14:05:53 +0530

    Update fix_dup_rpm.sh

--- fix_dup_rpm.sh
+++ fix_dup_rpm.sh
@@ -3,67 +3,85 @@
 
 verbose=0
 verbose=0
+count_dup=0
+count_rm=0
+
 # Command line arg handling #
+tempdirname=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1`
+mkdir /tmp/$tempdirname/
+DIR="/tmp/$tempdirname/"
+FILENAME="rpm-dup.txt"
 
 args=( "$@" )
 numarg=$#
 argnum=$((numarg-1))
 
 for i in `seq 0 "$argnum"`
-	do
-	key=${args[$i]}
-	case $key in
-    		-h|--help)
+        do
+        key=${args[$i]}
+        case $key in
+                -h|--help)
 
-			help="1"
-		;;
-    		-d|--dry-run)
+                        help="1"
+                ;;
+                -d|--dry-run)
 
-			dryrun="1"
-		;;
-		-v|--verbose)
+                        dryrun="1"
+                ;;
+                -v|--verbose)
 
-			verbose="1"
-		;;
+                        verbose="1"
+                ;;
 
- 		*)
-    		;;
-	esac
+                *)
+                ;;
+        esac
 done
 
 
 
-for i in $(rpm -qa --qf "%{name}.%{arch}\n"|sort |uniq|egrep -iv "kernel|glibc|gpg-pubkey" ) ; 
-  do  
-  count=`rpm -q $i|wc -l`; 
-  if [ $count -gt 1 ]; then 
-    echo "dups detected: $i" ;
-    echo ------------- ; 
-    rpm=`rpm -q $i|sort|head -1` ; 
+for i in $(rpm -qa --qf "%{name}.%{arch}\n"|sort |uniq|egrep -iv "kernel|glibc|gpg-pubkey" ) ;
+  do
+  count=`rpm -q $i|wc -l`;
+  if [ $count -gt 1 ]; then
+    echo "dups detected: $i" |tee -a $DIR/$FILENAME  ;
+    count_dup=$((count_dup+1))
+    echo ------------- |tee -a $DIR/$FILENAME ;
+    rpm=`rpm -q $i|sort|head -1` ;
     if [ "$dryrun" == "1" ]; then
-      
-      echo "Listing duplicates for $i ..."  ; 
-      rpm -q $i; echo ---------------   ;      
-      echo "$rpm will be removed"
-      echo "============================"
+
+      echo "Listing duplicates for $i ..."  |tee -a $DIR/$FILENAME ;
+      rpm -q $i; echo ---------------   |tee -a $DIR/$FILENAME ;
+      echo "$rpm will be removed" |tee -a $DIR/$FILENAME
+      echo "============================" |tee -a $DIR/$FILENAME
     else
-      echo "Attempting to remove $rpm ..." ; 
-      rpm -ev --nodeps $rpm && echo "$rpm removed successfully" || echo "$rpm remove failed"; 
-      echo "Checking list after removal..."  ; 
-      rpm -q $i; 
-      echo "============================"
+      echo "Attempting to remove $rpm ..." |tee -a $DIR/$FILENAME ;
+      count_rm=$((count_rm+1))
+      rpm -ev --nodeps $rpm && echo "$rpm removed successfully"|tee -a $DIR/$FILENAME  || echo "$rpm remove failed"|tee -a $DIR/$FILENAME ;
+      echo "Checking list after removal..."  |tee -a $DIR/$FILENAME ;
+      rpm -q $i;
+      echo "============================"|tee -a $DIR/$FILENAME
 
     fi
-  else 
+  else
     if [ "$verbose" == "1" ]; then
-      echo "No dups found: $i" ; 
-      echo -------------------; 
+      echo "No dups found: $i" |tee -a $DIR/$FILENAME ;
+      echo -------------------|tee -a $DIR/$FILENAME ;
     fi
-  fi 
+  fi
   done
 
-    if [ "$dryrun" == "0" ]  ; then
-	echo "Running package-cleanup --dupes after cleanup..."
-	package-cleanup --dupes
+    if [ "$dryrun" == "0" ]   ; then
+        echo "Running package-cleanup --dupes after cleanup..."|tee -a $DIR/$FILENAME
+        package-cleanup --dupes|tee -a $DIR/$FILENAME
     fi
-
+echo
+echo "Summary"
+echo "-----------"
+echo "Found $count duplicate rpms"
+if [ "$dryrun" == "0" ]   ; then
+	echo "DRY RUN MODE: No changes has been performed on the system..."
+else
+	echo "Removed $count_rm" rpms
+fi
+echo "Summary has been saved in $DIR/$FILENAME"

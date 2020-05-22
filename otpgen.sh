9633c0bb4e84710d14f31bb36328b4404243dac4
Shatadru Bandyopadhyay <shatadru1@gmail.com>
2020-05-23 03:50:36 +0530

    [otpgen.sh] Added licence
    - added licence
    - modified supported and tested OS list

--- otpgen.sh
+++ otpgen.sh
@@ -2,9 +2,28 @@
 
 # Author : Shatadru Bandyopadhyay
 #           shatadru1@gmail.com
-# Supports : ubuntu, debian, Fedora, RHEL
-# Services tested so far
-# amazon.in + Fedora 30
+# Supports : Fedora, Ubuntu (more to be added)
+# Services tested so far : amazon.in
+# Tested OS:  Fedora 30,31,32
+
+# OBTAIN THE LATEST VERSION OF THE SCRIPT AT :  https://github.com/shatadru/simpletools/blob/master/otpgen.sh
+#                       DIRECT DOWNLOAD LINK : https://raw.githubusercontent.com/shatadru/simpletools/master/otpgen.sh
+
+# 
+# Licenced under GPLv3, check LICENSE.txt
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 export HOME=$(bash <<< "echo ~${SUDO_USER:-}")
 os=""

ff4df7e6f02cdfcdf5f9a7cf8ea55a4b71f8621a
Shatadru Bandyopadhyay <shatadru1@gmail.com>
2020-08-01 21:15:15 +0530

    [CI] Minor change to make Debian CI work - Put the time command before printf

--- ci/gen_key.sh
+++ ci/gen_key.sh
@@ -5,8 +5,8 @@ printf 'StorngPass123!\n'|bash -x ./otpgen/otpgen.sh -l
 
 
 echo "Generating key for ID 1"
-printf 'StorngPass123!\n'|time bash -x ./otpgen/otpgen.sh -g 1
+time printf 'StorngPass123!\n'| bash -x ./otpgen/otpgen.sh -g 1
 
 
 echo "Generating key for ID 2"
-printf 'StorngPass123!\n'|time bash -x ./otpgen/otpgen.sh -g 2
+time printf 'StorngPass123!\n'| bash -x ./otpgen/otpgen.sh -g 2
